58666d8478c3313680d438f6b3ace2605e8df4c8
Feng <fengparra@outlook.com>
2022-08-03 12:42:55 -0400

    Addition works Equals button initiates addition based on numbers added to the queue.

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
 function createNumBtns() {
-    const numbers = document.querySelectorAll('.btn-num');
+    const numbers = document.querySelectorAll('button');
 
     numbers.forEach(number => {
         number.addEventListener('click', getNumber);
-        number.addEventListener('click', displayNumber);
+        number.addEventListener('click', updateResult);
     });
 }
-
 
 function getNumber() {
     queue.push(this.textContent);
 }
 
-function displayNumber() {
+function updateResult() {
     const results = document.querySelector('h2');
     let equation = queue.join('');
 
     results.textContent = equation;
 }
 
-let queue = [];
+function add(...nums) {
+    let result = 0;
 
+    nums.forEach(num => {
+        result += num;
+    })
+    return result;
+}
+
+function parseQueue() {
+    let num = '';
+    let nums = [];
+    let ops = [];
+
+    while (queue.length != 0) {
+        if (queue[0].match(/[+\-*\/=]/g)) {
+            nums.push(Number(num));
+            num = '';
+            ops.push(queue.shift());
+        }
+        num += queue.shift();
+    }
+    equate(nums, ops);
+}
+
+function equate(nums, ops) {
+    if (ops[0] === '+') 
+        queue.push(add(nums.shift(), nums.shift()));
+    updateResult()
+    queue = [];
+}
+
+let queue = [];
 
 createNumBtns();
 
+const equalBtn = document.querySelector('.equal-btn');
+equalBtn.addEventListener('click', parseQueue);
